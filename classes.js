98c28ba6a61ff634c6ff44c9d78ec5c94d588b31
Deepak Mathpal <deemathp@publicisgroupe.net>
2019-08-01 18:48:11 +0530

    clases concept

--- classes.js
+++ classes.js
@@ -1,7 +1,15 @@
 var Person = /** @class */ (function () {
     function Person() {
+        this.firstName = "deepak";
+        this.lastName = "math";
     }
+    Person.prototype.getfullName = function () {
+        return this.firstName + '' + this.lastName;
+    };
     return Person;
 }());
 var aPerson = new Person();
-console.log(aPerson.firstName = "deepak");
+aPerson.firstName = "deepak";
+aPerson.lastName = "Mathpal";
+console.log(aPerson.getfullName());
+console.log(aPerson);
--- classes.ts
+++ classes.ts
@@ -2,8 +2,20 @@ class Person {
 
     firstName:string;
     lastName:string;
+    constructor()
+    {
+        this.firstName = `deepak`;
+        this.lastName = `math`;
+
+    }
+    getfullName(){
+        return this.firstName + '' + this.lastName;
+    }
 
 }
 
 var aPerson = new Person();
-console.log(aPerson.firstName = "deepak");
+aPerson.firstName = "deepak";
+aPerson.lastName = "Mathpal";
+console.log(aPerson.getfullName());
+console.log(aPerson);
